a3ade425545386a836b67215b262a53666ccff6f
ameyyadav09 <ameykumar.tkr@gmail.com>
2015-12-01 14:12:29 +0530

    user now has a playlist

--- mymusic/models.py
+++ mymusic/models.py
@@ -21,3 +21,10 @@ class Songs(models.Model):
 	artist = models.CharField(max_length = 100)
 	album = models.CharField(max_length = 100)
 	language = models.CharField(max_length=20 ,unique = True)
+
+class UserPlaylist(models.Model):
+	user = models.ForeignKey(Userdetails)
+	songName = models.ForeignKey(Songs)
+
+class Session(models.Model):
+	name = models.EmailField(max_length=200,unique=True)
